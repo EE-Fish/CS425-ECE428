9cfd8795aef0cd864e049f9b98ced2b4985d33b5
TIAN CHEN <826218335@qq.com>
2019-04-17 11:36:10 -0500

    Add files via upload

---
+++ MP2/run.sh
@@ -0,0 +1,10 @@
+go run MP2.go node1 10.182.188.148 5555 > node1.log &
+go run MP2.go node2 10.182.188.148 5556 > node2.log &
+go run MP2.go node3 10.182.188.148 5557 > node3.log &
+go run MP2.go node4 10.182.188.148 5558 > node4.log &
+go run MP2.go node5 10.182.188.148 5559 > node5.log &
+go run MP2.go node6 10.182.188.148 5560 > node6.log &
+go run MP2.go node7 10.182.188.148 5561 > node7.log &
+go run MP2.go node8 10.182.188.148 5562 > node8.log &
+go run MP2.go node9 10.182.188.148 5563 > node9.log &
+go run MP2.go node10 10.182.188.148 5564 > node10.log
